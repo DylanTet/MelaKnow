daad095d0aaecd669dd587f16d74fcc4eb4209d5
Dylan <tettemerd@gmail.com>
2023-04-24 18:36:46 -0700

    Save the photos to custom folder locally

--- src/infrastructure/screens/PictureLibraryScreen.tsx
+++ src/infrastructure/screens/PictureLibraryScreen.tsx
@@ -1,5 +1,5 @@
 import { Image, SafeAreaView } from 'react-native'
-import { collection, getDocs, getFirestore, doc } from 'firebase/firestore'
+import { collection, getFirestore, doc, getDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { db } from '../../services/firebase'
 
@@ -12,13 +12,7 @@ const PictureLibraryScreen: React.FC<Props> = ({ userId }) => {
   const [photos, setPhotos] = useState<Image[]>([])
 
   useEffect(() => {
-    const fetchPhotos = async () => {
-      const pictureDocReference = await doc(db, "userPictures", userId)
-        .then((pictureData) => {
-          pictureData.
-        })
-        .catch()
-    }
+    
   })
   return (
     <SafeAreaView className='flex-1'>
