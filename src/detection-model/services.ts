9bcf34911a2fc154302fcf17c7caa18ae7c05340
Dylan <tettemerd@gmail.com>
2023-11-20 20:58:24 -0800

    added custom ioHandler to work with expo

--- src/detection-model/services.ts
+++ src/detection-model/services.ts
@@ -1,21 +1,83 @@
 import * as tf from '@tensorflow/tfjs'
+import { io, util } from '@tensorflow/tfjs-core'
+import {Asset} from 'expo-asset';
+import { Platform } from 'react-native';
 import {bundleResourceIO, decodeJpeg} from '@tensorflow/tfjs-react-native'
 import * as FileSystem from 'expo-file-system';
 
-const loadModel = async () : Promise<void | tf.LayersModel> => {
-    const modelJSON = require('./model-assets/model.json');
-    const modelWeights = await require('./model-assets/group1-shard1of1.bin');
+const modelJSON = require('./model-assets/model.json');
+const modelWeights = require('./model-assets/group1-shard1of1.bin');
 
-    if (!modelJSON || !modelWeights) {
-        throw new Error("Failed to load the model files required.")
+class customIOHandler implements io.IOHandler {
+    constructor(
+        protected readonly modelJson: io.ModelJSON,
+        protected readonly modelWeightsId: Array<string|number>) {
+            if (modelJson == null || modelWeightsId == null) {
+                throw new Error(
+                    'Must pass the model json object and the model weights path.'
+                );
+            }
+        }
+    
+    async load() : Promise<io.ModelArtifacts> {
+        const weightAssets = this.modelWeightsId.map(id => Asset.fromModule(id));
+        return this.loadLocalAsset(weightAssets);
     }
 
-    const model = await tf.loadLayersModel(
-        bundleResourceIO(modelJSON, modelWeights)
-        
-    ).then((m) => {
-        return m
-    }).catch((err) => console.log('There was an error loading the model', err))
+    loadLocalAsset = async (weights : Asset[]) : Promise<io.ModelArtifacts> => {
+        const modelJson = this.modelJson;
+        const modelArtifacts: io.ModelArtifacts = Object.assign({}, modelJson);
+        modelArtifacts.weightSpecs = modelJson.weightsManifest[0].weights;
+        //@ts-ignore
+        delete modelArtifacts.weightManifest;
+    
+        const weightsArray = await Promise.all(weights.map(async (weightAssets) => {
+            let base64Weights: string;
+            if (Platform.OS === 'android') {
+                // On android we get a resource id instead of a regular path. We
+                // need to load the weights from the res/raw folder using this id.
+                const fileName = `${weightAssets.uri}.${weightAssets.type}`;
+                try {
+                  const returnedBase64Weights = await FileSystem.readAsStringAsync(fileName)
+                  base64Weights = returnedBase64Weights[0];
+                } catch (e) {
+                  throw new Error(
+                      `Error reading resource ${fileName}. Make sure the file is
+                in located in the res/raw folder of the bundle`,
+                  );
+                }
+            } else {
+                try {
+                  const returnedBase64Weights = await FileSystem.readAsStringAsync(weightAssets.uri.split('?')[0])
+                  base64Weights = returnedBase64Weights[0];
+                } catch (e) {
+                  throw new Error(
+                      `Error reading resource ${weightAssets.uri.split('?')[0]}.`,
+                  );
+                }
+            }
+            const weightData = util.encodeString(base64Weights, 'base64').buffer;
+            return weightData;
+        }));
+    
+        modelArtifacts.weightData = io.CompositeArrayBuffer.join(weightsArray);
+        console.log(modelArtifacts);
+        return modelArtifacts;
+    }
+}
+
+const customBundleResourceIO = (modelJson: io.ModelJSON, modelWeightsId: number|number[]): io.IOHandler => {
+    const modelWeightsIdArr = Array.isArray(modelWeightsId) ? modelWeightsId : [modelWeightsId];
+    return new customIOHandler(modelJson, modelWeightsIdArr);
+}
+
+const loadModel = async () : Promise<void | tf.LayersModel> => {
+    try {
+        const model = await tf.loadLayersModel(customBundleResourceIO(modelJSON, modelWeights))
+        return model;
+    } catch(err) {
+        console.log("There was an error loading the model", err);
+    }
 }
 
 const predictFromPhoto = async (batch: number, model: tf.LayersModel, imagesTensor: tf.Tensor<tf.Rank>) : Promise<tf.Tensor<tf.Rank>[]> => {
