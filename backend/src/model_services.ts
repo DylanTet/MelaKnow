507c19be55004d822dc3e06e1f84b7100113ef86
Dylan <tettemerd@gmail.com>
2024-01-09 10:31:24 -0800

    Node successfully calls python process to predict photo

--- backend/src/model_services.ts
+++ backend/src/model_services.ts
@@ -1,39 +1,41 @@
 import * as tf from '@tensorflow/tfjs-node';
-import path from 'path'
+import path from 'path';
 import fs from 'fs';
+import { spawn } from 'child_process';
 
-export const loadModel = async () : Promise<void | tf.GraphModel> => {
-    try {
-        const modelJsonPath = path.join(process.cwd(), '/src/model_assets/model.json')
-        const handler = tf.io.fileSystem(modelJsonPath)
-        const model = await tf.loadGraphModel(handler);
-        return model;
-    } catch(err) {
-        console.log("There was an error loading the model", err);
-    }
-}
-
-export const predictFromPhoto = async (model: tf.GraphModel) : Promise<tf.Tensor | string | undefined>  => {
+export const predictFromPhoto = () : string | undefined  => {
     try {
         const uri = path.join(process.cwd(), 'src/utils/data/test_image/cancer-test.jpg');
         const imgBuffer = fs.readFileSync(uri);
-        let imgTensor = tf.node.decodeJpeg(imgBuffer, 3);
-        const resizedImage = imgTensor.resizeBilinear([299,299]);
-        const normalizedImgTensor = resizedImage.toFloat().div(tf.scalar(255));
-        const expandedImgTensor = normalizedImgTensor.expandDims(0);
+        const base64Img = imgBuffer.toString("base64");
 
-        const prediction = model.predict(expandedImgTensor) as tf.Tensor;
-        const string = prediction.arraySync().toString();
-        console.log(string)
-        return prediction;
+        const tempFilePath = path.join(process.cwd(), 'temp_image.txt');
+        fs.writeFileSync(tempFilePath, base64Img);
+
+        const pythonProcess = spawn('python3', ["-u", "./utils/model.py", tempFilePath]);
+        pythonProcess.stdout.on('data', (result) => {
+            const output = result.toString();
+            console.log("Data received");
+            return output;
+        })
+
+        pythonProcess.on('exit', (code) => {
+            console.log(`Python process exited with code ${code}`);
+          });
+
+        pythonProcess.stdin.on('error', (error) => {
+            console.error(`Error writing to python process: ${error.message}`)
+        })
+        
     } catch(err) {
         console.log("There was an error predicting image", err);
+        return undefined;
     }
 }
 
 export const getPrediction = async (imgBuffer: Uint8Array) => {
-    await tf.ready();
-    const model = await loadModel() as tf.GraphModel
-    const prediction = await predictFromPhoto(model);
+    // await tf.ready();
+    // const model = await loadModel() as tf.GraphModel
+    const prediction = await predictFromPhoto();
     return prediction;
 }
