dc207aef02def45c7bf7eff3f18fc74e87ff7950
Dylan <tettemerd@gmail.com>
2024-01-24 13:45:35 -0800

    frontend test for request to server added

--- frontend/tests/requestFromModelServer.test.js
+++ frontend/tests/requestFromModelServer.test.js
@@ -1,17 +1,50 @@
 import axios from 'axios';
 import { reqFromModelServer } from '../src/detection-model/services';
-import MockAdapter from 'axios-mock-adapter';
+import { EncodingType, readAsStringAsync } from 'expo-file-system';
+import { waitFor } from '@testing-library/react-native'
+
+jest.mock('expo-file-system', () => ({
+  ...jest.requireActual('expo-file-system'),
+  readAsStringAsync: jest.fn(() => Promise.resolve("Mock binary data"))
+}))
 
 describe('reqFromModelServer', () => {
-  it('returns prediction from server running python model', done => {
-    let mock = new MockAdapter(axios);
-    mock.onPost('https://localhost:4000/get-prediction').reply(200);
+  it('returns prediction successfully from server', async() => {
+    // Buffer.from = jest.fn(() => {
+    //   return Buffer("Mock Binary Data");
+    // });
 
-    const testPhotoPath = 
-    reqFromModelServer('/Users/dylantettemer/Desktop/projects/MelaKnow/frontend/tests/cancer-test.jpg').then(response => {
-      expect(response);
-      done();
-    })
+    axios.post = jest.fn(() => {
+      return Promise.resolve({
+        status: 200,
+        data: "Correct prediction"
+      })
+    });
+
+    await expect(reqFromModelServer('Path to user photo')).resolves.toEqual("Correct prediction");
+
+    await waitFor(() => {
+      expect(readAsStringAsync).toHaveBeenCalledWith("Path to user photo", { encoding: EncodingType.Base64 });
+      expect(axios.post).toHaveBeenCalled();
+    });
+  })
+
+  it('returns error from bad request', async() => {
+
+    axios.post = jest.fn(() => Promise.reject({
+      response: {
+        status: 400,
+        data: "Bad request",
+      },
+    }));
+
+    await expect(reqFromModelServer('Path to user photo')).rejects.toThrow("Error requesting to server");
+
+    await waitFor(() => {
+      expect(readAsStringAsync).toHaveBeenCalled();
+      expect(axios.post).toHaveBeenCalled();
+    });
+
   })
 })
 
